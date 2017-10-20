eb076454eba7bde96c2057bca6d344c69d109975
Wei WU <drink18@hotmail.com>
2017-10-19 21:55:45 -0400

    * implementation * packed vsix file

--- src/extension.ts
+++ src/extension.ts
@@ -14,11 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
+    let disposable = vscode.commands.registerCommand('extension.build', e => {
         // The code you place here will be executed every time your command is executed
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        let cmd = vscode.workspace.getConfiguration("rightclick").get("CommandToBind");
+        vscode.commands.executeCommand("workbench.action.tasks.runTask", cmd);
     });
 
     context.subscriptions.push(disposable);
